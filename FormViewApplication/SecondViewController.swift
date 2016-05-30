4dadb69f366f4ce70518ccd61f6a8a0eabe88a2d
bkkoo <bon@duotrac.com>
2016-05-30 16:43:15 +0530

    First commit

---
+++ FormViewApplication/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  FormViewApplication
+//
+//  Created by Chetu India on 30/05/16.
+//  Copyright © 2016 Chetu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ FormViewApplication/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FormViewApplication
+//
+//  Created by Chetu India on 30/05/16.
+//  Copyright © 2016 Chetu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
