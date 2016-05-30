6ab01e43207cfccb1de3de0cbcf7be7a75e3716a
bkkoo <bon@duotrac.com>
2016-05-30 17:10:46 +0530

    Third commit

---
+++ FormViewApplication/FifthViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FifthViewController.swift
+//  FormViewApplication
+//
+//  Created by Chetu India on 30/05/16.
+//  Copyright © 2016 Chetu. All rights reserved.
+//
+
+
+import UIKit
+
+class FifthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("Hi inside of an viewWillAppear method.")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
