3f86ca4bf461f77ae453b126cd658f74ad5da2b8
bkkoo <bon@duotrac.com>
2016-05-30 16:55:14 +0530

    Second commit

---
+++ FormViewApplication/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  FormViewApplication
+//
+//  Created by Chetu India on 30/05/16.
+//  Copyright © 2016 Chetu. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("Hi inside of an viewWillAppear method")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
